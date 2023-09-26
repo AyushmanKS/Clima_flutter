7b74841b82b5e6bac2bb1302ae69897303988c2c
AyushmanKS <ayushmanks.1845@gmail.com>
2023-09-26 18:23:24 +0530

    first commit

---
+++ clima/lib/screens/loading_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoadingScreenState();
+  }
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Geolocator.checkPermission();
+    Geolocator.requestPermission();
+    var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: SpinKitSpinningLines(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
